be3e4aee230199464c826b49925fe0a26373b17b
douglasnickson <douglas.nickson1@gmail.com>
2018-01-29 21:05:15 -0300

    Iniciado Formulario de Cadastro

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,13 @@
 $("ul.nav li.parent>a").click(function(){
     $(this).find('em').toggleClass("fa-minus");      
 });
+
+
+//Mostra ou Esconde a Sidebar de Acordo com a tela
+$(window).on('resize', function () {
+ 	if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+});
+
+$(window).on('resize', function () {
+	if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+});
