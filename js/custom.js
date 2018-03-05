413063e7b62dd0327354612a4605950856d02b5a
douglasnickson <douglas.nickson1@gmail.com>
2018-03-05 00:25:09 -0300

    Reformulação do Layout

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,16 @@
 
 //Alterando o Icone de + e - da sidebar
 $("ul.nav li.parent>a").click(function(){
+ 
     $(this).find('em').toggleClass("fa-minus");      
 });
 
 
 //Mostra ou Esconde a Sidebar de Acordo com a tela
 $(window).on('resize', function () {
- 	if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+ 	if ($(window).width() > 768) $('#main-sidebar').collapse('show')
 });
 
 $(window).on('resize', function () {
-	if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+	if ($(window).width() <= 767) $('#main-sidebar').collapse('hide')
 });
